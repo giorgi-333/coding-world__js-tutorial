c1cfd5906d8bd25a0325ba1713d9089f81b093ff
giorgi-333 <giorgienuqidze2000@gmail.com>
2022-08-24 11:46:05 +0400

    Removed extra comments

--- 26/js/main.js
+++ 26/js/main.js
@@ -1,18 +1,5 @@
 
 
-// const todoList = []
-
-// function addToTodo() {
-//     todoList.push(todoInput.value)
-    
-//     res.innerHTML = ""
-
-//     todoList.forEach((todo, i) => {
-//         res.innerHTML += `${i+1}) ${todo}<br>`
-//     })
-// }
-
-
 const places = ["თბილისი","გორი","ქსანი","თელავი"]
 
 let history = "ქსანი - სოფელი მცხეთის მუნიციპალიტეტში"
--- 27/js/main.js
+++ 27/js/main.js
@@ -1,24 +1,5 @@
 
 
-// const todoList = []
-
-// function addToTodo() {
-//     todoList.push(todoInput.value)
-    
-//     res.innerHTML = ""
-
-//     todoList.forEach((todo, i) => {
-//         res.innerHTML += `${i+1}) ${todo}<br>`
-//     })
-// }
-
-
-// const places = ["თბილისი","გორი","ქსანი","თელავი"]
-
-// let history = "ქსანი - სოფელი მცხეთის მუნიციპალიტეტში"
-
-// console.log(history.includes("სოფელ"))
-
 
 const numbers = [1,4,6,12,34,12,45]
 
